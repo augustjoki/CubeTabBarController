891bc311e5d46c0fafe9b6084f890401631602d8
August Joki <august.joki@gmail.com>
2011-07-21 21:17:25 -0700

    Initial Commit

---
+++ CubeTabBarController/CubeTabBarControllerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CubeTabBarControllerAppDelegate.h
+//  CubeTabBarController
+//
+//  Created by August Joki on 7/21/11.
+//  Copyright 2011 Concinnous Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CubeTabBarControllerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CubeTabBarController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CubeTabBarController
+//
+//  Created by August Joki on 7/21/11.
+//  Copyright 2011 Concinnous Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CubeTabBarControllerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([CubeTabBarControllerAppDelegate class]));
+	}
+}
